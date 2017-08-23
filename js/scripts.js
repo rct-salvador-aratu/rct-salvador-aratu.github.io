a3568045226e3caffb5bc99231477de1cf1dc2ad
Luis Ferreira <luisrbferreira@gmail.com>
2017-08-23 01:29:55 -0300

    Navbar e intro

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
     $(function(){
 
         $('.button-collapse').sideNav();
-
+        $('.carousel.carousel-slider').carousel({fullWidth: true});
+        $('.slider').slider();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
